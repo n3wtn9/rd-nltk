d0e7d264fd3b57720781515dc9d6e5604fc669ff
Newton Truong <newton@qurius-inc.com>
2016-04-24 16:21:51 -0700

    init

---
+++ app/script/neg_corpus.py
@@ -0,0 +1,15 @@
+from nltk.sentiment.util import *
+from nltk.tokenize import sent_tokenize
+import os
+
+root_input = './data/input'
+root_output = './data/output'
+
+for filename in os.listdir(root_input):
+    with open(os.path.join(root_input, filename)) as fin:
+        doc = fin.read()
+        sent_tokenize_list = sent_tokenize(doc)
+        marked = " ".join([" ".join(mark_negation(sent.split())) for sent in sent_tokenize_list])
+
+        with open(os.path.join(root_output, filename), 'w') as fout:
+            fout.write(marked)
